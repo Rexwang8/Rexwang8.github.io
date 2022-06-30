2cadf53203be31dcbb1520d4decb1f92e2004012
Rexwang8 <rexkingsbackyard@gmail.com>
2022-06-30 02:27:55 -0400

    trying to get subdomains to work

--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ import RoutingPage from './pages/RoutingPage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename=''>
     <ThemeProvider>
     <RoutingPage />
     </ThemeProvider>
     
-    </BrowserRouter>
+    </BrowserRouter >
    
   </React.StrictMode>
 );

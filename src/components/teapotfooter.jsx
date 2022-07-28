40f7df4f12e1f050ff1b24b15645f848c1e032aa
rexwangmarquis <rexwang@marquisenergy.com>
2022-07-28 15:49:12 -0500

    TONS of landscapes

--- src/components/teapotfooter.jsx
+++ src/components/teapotfooter.jsx
@@ -1,7 +1,7 @@
 function TeapotFooter() {
   return (
     <div className='footer'>
-      <a href='https://ko-fi.com/J3J1E24MB' target='_blank'>
+      <a href='https://ko-fi.com/J3J1E24MB' target='_blank' rel="noreferrer">
         <img height='36' src='https://cdn.ko-fi.com/cdn/kofi2.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' />
       </a>
       <p>Portfolio Website for Rex Wang, Coded in React framework in 2022.</p>
